39c720e62ad47ced647462992fa7101af453ba43
Dave DeSandro <desandrocodes@gmail.com>
2021-12-24 23:15:06 -0500

    reorganize methods

--- unidragger.js
+++ unidragger.js
@@ -22,6 +22,21 @@
 
 }( window, function factory( window, EvEmitter ) {
 
+function Unidragger() {}
+
+// inherit EvEmitter
+let proto = Unidragger.prototype = Object.create( EvEmitter.prototype );
+
+// ----- bind start ----- //
+
+// trigger handler methods for events
+proto.handleEvent = function( event ) {
+  let method = 'on' + event.type;
+  if ( this[ method ] ) {
+    this[ method ]( event );
+  }
+};
+
 let startEvent, activeEvents;
 if ( 'ontouchstart' in window ) {
   // HACK prefer Touch Events as you can preventDefault on touchstart to
@@ -38,20 +53,44 @@ if ( 'ontouchstart' in window ) {
   activeEvents = [ 'mousemove', 'mouseup' ];
 }
 
-// -------------------------- Unidragger -------------------------- //
+// prototype so it can be overwriteable by Flickity
+proto.touchActionValue = 'none';
 
-function Unidragger() {}
-
-// inherit EvEmitter
-let proto = Unidragger.prototype = Object.create( EvEmitter.prototype );
-
-// trigger handler methods for events
-proto.handleEvent = function( event ) {
-  let method = 'on' + event.type;
-  if ( this[ method ] ) {
-    this[ method ]( event );
-  }
+proto.bindHandles = function() {
+  this._bindHandles( 'addEventListener', this.touchActionValue );
 };
+
+proto.unbindHandles = function() {
+  this._bindHandles( 'removeEventListener', '' );
+};
+
+/**
+ * Add or remove start event
+ * @param {String} bindMethod - addEventListener or removeEventListener
+ * @param {String} touchAction - value for touch-action CSS property
+ */
+proto._bindHandles = function( bindMethod, touchAction ) {
+  this.handles.forEach( ( handle ) => {
+    handle[ bindMethod ]( startEvent, this );
+    handle[ bindMethod ]( 'click', this );
+    // touch-action: none to override browser touch gestures. metafizzy/flickity#540
+    if ( window.PointerEvent ) handle.style.touchAction = touchAction;
+  } );
+};
+
+proto.bindActivePointerEvents = function() {
+  activeEvents.forEach( ( eventName ) => {
+    window.addEventListener( eventName, this );
+  } );
+};
+
+proto.unbindActivePointerEvents = function() {
+  activeEvents.forEach( ( eventName ) => {
+    window.removeEventListener( eventName, this );
+  } );
+};
+
+// ----- event handler helpers ----- //
 
 // trigger method with matching pointer
 proto.withPointer = function( methodName, event ) {
@@ -85,17 +124,23 @@ proto.onpointerdown = function( event ) {
   this.pointerDown( event, event );
 };
 
+// nodes that have text fields
+const cursorNodes = [ 'TEXTAREA', 'INPUT', 'SELECT', 'OPTION' ];
+// input types that do not have text fields
+const clickTypes = [ 'radio', 'checkbox', 'button', 'submit', 'image', 'file' ];
+
 /**
- * pointer start
+ * any time you set `event, pointer` it refers to:
  * @param {Event} event
  * @param {Event | Touch} pointer
  */
 proto.pointerDown = function( event, pointer ) {
-  // dismiss right click and other pointers
-  // button = 0 is okay, 1-4 not
-  if ( event.button || this.isPointerDown ) {
-    return;
-  }
+  // dismiss multi-touch taps, right clicks, and clicks on text fields
+  let isCursorNode = cursorNodes.includes( event.target.nodeName );
+  let isClickType = clickTypes.includes( event.target.type );
+  let isOkayElement = !isCursorNode || isClickType;
+  let isOkay = !this.isPointerDown && !event.button && isOkayElement;
+  if ( !isOkay ) return;
 
   this.isPointerDown = true;
   // save pointer identifier to match up touch events
@@ -104,26 +149,11 @@ proto.pointerDown = function( event, pointer ) {
     pointer.pointerId : pointer.identifier;
 
   this.pointerDown( event, pointer );
+  this.bindActivePointerEvents();
   this.emitEvent( 'pointerDown', [ event, pointer ] );
 };
 
-// proto.pointerDown = function( event, pointer ) {
-//   this.bindActivePointerEvents( event );
-// };
-
-proto.bindActivePointerEvents = function() {
-  activeEvents.forEach( ( eventName ) => {
-    window.addEventListener( eventName, this );
-  } );
-};
-
-proto.unbindActivePointerEvents = function() {
-  activeEvents.forEach( ( eventName ) => {
-    window.removeEventListener( eventName, this );
-  } );
-};
-
-// ----- move event ----- //
+// ----- move ----- //
 
 proto.onmousemove = function( event ) {
   this.pointerMove( event, event );
@@ -137,17 +167,36 @@ proto.ontouchmove = function( event ) {
   this.withTouch( 'pointerMove', event );
 };
 
-/**
- * pointer move
- * @param {Event} event
- * @param {Event | Touch} pointer
- * @private
- */
 proto.pointerMove = function( event, pointer ) {
-  this.emitEvent( 'pointerMove', [ event, pointer ] );
+  let moveVector = {
+    x: pointer.pageX - this.pointerDownPointer.pageX,
+    y: pointer.pageY - this.pointerDownPointer.pageY,
+  };
+  this.emitEvent( 'pointerMove', [ event, pointer, moveVector ] );
+  // start drag if pointer has moved far enough to start drag
+  let isDragStarting = !this.isDragging && this.hasDragStarted( moveVector );
+  if ( isDragStarting ) this.dragStart( event, pointer );
+  if ( this.isDragging ) this.dragMove( event, pointer, moveVector );
 };
 
-// ----- end event ----- //
+// condition if pointer has moved far enough to start drag
+proto.hasDragStarted = function( moveVector ) {
+  return Math.abs( moveVector.x ) > 3 || Math.abs( moveVector.y ) > 3;
+};
+
+// ----- drag ----- //
+
+proto.dragStart = function( event, pointer ) {
+  this.isDragging = true;
+  this.isPreventingClicks = true; // set flag to prevent clicks
+  this.emitEvent( 'dragStart', [ event, pointer ] );
+};
+
+proto.dragMove = function( event, pointer, moveVector ) {
+  this.emitEvent( 'dragMove', [ event, pointer, moveVector ] );
+};
+
+// ----- end ----- //
 
 proto.onmouseup = function( event ) {
   this.pointerUp( event, event );
@@ -161,32 +210,35 @@ proto.ontouchend = function( event ) {
   this.withTouch( 'pointerUp', event );
 };
 
-/**
- * pointer up
- * @param {Event} event
- * @param {Event | Touch} pointer
- */
 proto.pointerUp = function( event, pointer ) {
   this.pointerDone();
   this.emitEvent( 'pointerUp', [ event, pointer ] );
+
+  if ( this.isDragging ) {
+    this.dragEnd( event, pointer );
+  } else {
+    // pointer didn't move enough for drag to start
+    this.staticClick( event, pointer );
+  }
 };
 
-// ----- pointer done ----- //
+proto.dragEnd = function( event, pointer ) {
+  this.isDragging = false; // reset flag
+  // re-enable clicking async
+  setTimeout( () => delete this.isPreventingClicks );
+
+  this.emitEvent( 'dragEnd', [ event, pointer ] );
+};
 
 // triggered on pointer up & pointer cancel
 proto.pointerDone = function() {
-  this.pointerReset();
+  this.isPointerDown = false;
+  delete this.pointerIdentifier;
   this.unbindActivePointerEvents();
   this.emitEvent('pointerDone');
 };
 
-proto.pointerReset = function() {
-  // reset properties
-  this.isPointerDown = false;
-  delete this.pointerIdentifier;
-};
-
-// ----- pointer cancel ----- //
+// ----- cancel ----- //
 
 proto.onpointercancel = function( event ) {
   this.withPointer( 'pointerCancel', event );
@@ -196,191 +248,33 @@ proto.ontouchcancel = function( event ) {
   this.withTouch( 'pointerCancel', event );
 };
 
-/**
- * pointer cancel
- * @param {Event} event
- * @param {Event | Touch} pointer
- */
 proto.pointerCancel = function( event, pointer ) {
   this.pointerDone();
   this.emitEvent( 'pointerCancel', [ event, pointer ] );
 };
 
-// ----- bind start ----- //
-
-// prototype so it can be overwriteable by Flickity
-proto.touchActionValue = 'none';
-
-proto.bindHandles = function() {
-  this._bindHandles( 'addEventListener', this.touchActionValue );
-};
-
-proto.unbindHandles = function() {
-  this._bindHandles( 'removeEventListener', '' );
-};
-
-/**
- * Add or remove start event
- * @param {String} bindMethod - addEventListener or removeEventListener
- * @param {String} touchAction - value for touch-action CSS property
- */
-proto._bindHandles = function( bindMethod, touchAction ) {
-  this.handles.forEach( ( handle ) => {
-    handle[ bindMethod ]( startEvent, this );
-    handle[ bindMethod ]( 'click', this );
-    // touch-action: none to override browser touch gestures. metafizzy/flickity#540
-    if ( window.PointerEvent ) handle.style.touchAction = touchAction;
-  } );
-};
-
-// ----- start event ----- //
-
-// nodes that have text fields
-const cursorNodes = [ 'TEXTAREA', 'INPUT', 'SELECT', 'OPTION' ];
-
-// input types that do not have text fields
-const clickTypes = [ 'radio', 'checkbox', 'button', 'submit', 'image', 'file' ];
-
-// dismiss inputs with text fields. flickity#403, flickity#404
-proto.okayPointerDown = function( event ) {
-  let isCursorNode = cursorNodes.includes( event.target.nodeName );
-  let isClickType = clickTypes.includes()[ event.target.type ];
-  let isOkay = !isCursorNode || isClickType;
-  if ( !isOkay ) {
-    this.pointerReset();
-  }
-  return isOkay;
-};
-
-// blur previously focused input
-// proto.pointerDownBlur = function() {
-//   document.activeElement.blur()
-// };
-
-// ----- move event ----- //
-
-/**
- * drag move
- * @param {Event} event
- * @param {Event | Touch} pointer
- */
-proto.pointerMove = function( event, pointer ) {
-  let moveVector = this._dragPointerMove( event, pointer );
-  this.emitEvent( 'pointerMove', [ event, pointer, moveVector ] );
-  this._dragMove( event, pointer, moveVector );
-};
-
-// base pointer move logic
-proto._dragPointerMove = function( event, pointer ) {
-  let moveVector = {
-    x: pointer.pageX - this.pointerDownPointer.pageX,
-    y: pointer.pageY - this.pointerDownPointer.pageY,
-  };
-  // start drag if pointer has moved far enough to start drag
-  if ( !this.isDragging && this.hasDragStarted( moveVector ) ) {
-    this._dragStart( event, pointer );
-  }
-  return moveVector;
-};
-
-// condition if pointer has moved far enough to start drag
-proto.hasDragStarted = function( moveVector ) {
-  return Math.abs( moveVector.x ) > 3 || Math.abs( moveVector.y ) > 3;
-};
-
-// ----- end event ----- //
-
-/**
- * pointer up
- * @param {Event} event
- * @param {Event | Touch} pointer
- */
-proto.pointerUp = function( event, pointer ) {
-  this.emitEvent( 'pointerUp', [ event, pointer ] );
-  this._dragPointerUp( event, pointer );
-};
-
-proto._dragPointerUp = function( event, pointer ) {
-  if ( this.isDragging ) {
-    this._dragEnd( event, pointer );
-  } else {
-    // pointer didn't move enough for drag to start
-    this.staticClick( event, pointer );
-  }
-};
-
-// -------------------------- drag -------------------------- //
-
-// dragStart
-proto._dragStart = function( event, pointer ) {
-  this.isDragging = true;
-  // prevent clicks
-  this.isPreventingClicks = true;
-  this.dragStart( event, pointer );
-};
-
-proto.dragStart = function( event, pointer ) {
-  this.emitEvent( 'dragStart', [ event, pointer ] );
-};
-
-// dragMove
-proto._dragMove = function( event, pointer, moveVector ) {
-  // do not drag if not dragging yet
-  if ( !this.isDragging ) {
-    return;
-  }
-
-  this.dragMove( event, pointer, moveVector );
-};
-
-proto.dragMove = function( event, pointer, moveVector ) {
-  event.preventDefault();
-  this.emitEvent( 'dragMove', [ event, pointer, moveVector ] );
-};
-
-// dragEnd
-proto._dragEnd = function( event, pointer ) {
-  // set flags
-  this.isDragging = false;
-  // re-enable clicking async
-  setTimeout( () => {
-    delete this.isPreventingClicks;
-  } );
-
-  this.dragEnd( event, pointer );
-};
-
-proto.dragEnd = function( event, pointer ) {
-  this.emitEvent( 'dragEnd', [ event, pointer ] );
-};
-
-// ----- onclick ----- //
+// ----- click ----- //
 
 // handle all clicks and prevent clicks when dragging
 proto.onclick = function( event ) {
-  if ( this.isPreventingClicks ) {
-    event.preventDefault();
-  }
+  if ( this.isPreventingClicks ) event.preventDefault();
 };
-
-// ----- staticClick ----- //
 
 // triggered after pointer down & up with no/tiny movement
 proto.staticClick = function( event, pointer ) {
   // ignore emulated mouse up clicks
-  if ( this.isIgnoringMouseUp && event.type == 'mouseup' ) {
-    return;
-  }
+  let isMouseup = event.type == 'mouseup';
+  if ( isMouseup && this.isIgnoringMouseUp ) return;
 
   this.emitEvent( 'staticClick', [ event, pointer ] );
 
   // set flag for emulated clicks 300ms after touchend
-  if ( event.type != 'mouseup' ) {
+  if ( isMouseup ) {
     this.isIgnoringMouseUp = true;
-    // reset flag after 300ms
-    setTimeout( function() {
+    // reset flag after 400ms
+    setTimeout( () => {
       delete this.isIgnoringMouseUp;
-    }.bind( this ), 400 );
+    }, 400 );
   }
 };
 
